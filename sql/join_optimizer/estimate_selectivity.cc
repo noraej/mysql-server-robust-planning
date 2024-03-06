77f218579744d9ed25233213a344d709ef96fe6b
Lea Grønning <leagrenning@hotmail.com>
2024-03-06 09:06:20 +0100

    Cleanup

--- sql/join_optimizer/estimate_selectivity.cc
+++ sql/join_optimizer/estimate_selectivity.cc
@@ -315,11 +315,10 @@ KeySelectivityResult EstimateSelectivityFromIndexStatistics(
 double EstimateEqualPredicateSelectivity(const EqualFieldArray &equal_fields,
                                          const CompanionSet &companion_set,
                                          string *trace) {
-  //Note Lea: Do not need to calculate new estimate since the method that calls this method does the calculation
+  //Note leag: Do not need to calculate new estimate since the method that calls this method does the calculation
   uint longest_prefix = 0;
   double selectivity = -1.0;
   double selectivity_cap = 1.0;
-  //RiskLevel risk_level = RiskLevel::High;
 
   for (const Field *equal_field : equal_fields) {
     for (uint key_no = equal_field->part_of_key.get_first_set();
@@ -341,10 +340,8 @@ double EstimateEqualPredicateSelectivity(const EqualFieldArray &equal_fields,
       if (key_data.prefix_length > longest_prefix) {
         longest_prefix = key_data.prefix_length;
         selectivity = key_data.selectivity;
-        //risk_level = RiskLevel::Low;
       } else if (key_data.prefix_length == longest_prefix) {
         selectivity = std::max(selectivity, key_data.selectivity);
-        //risk_level = RiskLevel::Low;
       }
     }
   }
@@ -355,12 +352,10 @@ double EstimateEqualPredicateSelectivity(const EqualFieldArray &equal_fields,
     // Look for histograms if there was no suitable index.
     for (const Field *field : equal_fields) {
       selectivity = std::max(selectivity, HistogramSelectivity(*field, trace));
-      //risk_level = RiskLevel::Medium;
     }
   }
 
   return selectivity;
-  //return BoundingBox::GetNewEstimate(selectivity, risk_level);
 }
 
 }  // Anonymous namespace.
