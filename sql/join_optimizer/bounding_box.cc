ea883f72beebc592ddcf2a67816e201b92945ed2
Lea Grønning <leagrenning@hotmail.com>
2024-04-23 12:03:47 +0200

    Cleanup

--- sql/join_optimizer/bounding_box.cc
+++ sql/join_optimizer/bounding_box.cc
@@ -4,16 +4,8 @@
 #include "sql/join_optimizer/bounding_box.h"
 #include <type_traits>
 
-double BoundingBox::incremental_step = 0.1;
-double BoundingBox::decremental_step = 0.05;
-double BoundingBox::alpha = 0.7;
-double BoundingBox::beta = 0.2;
-double BoundingBox::gamma = 0.1;
+double BoundingBox::incremental_step = 0.15;
 
 double BoundingBox::GetUpperBound(const double estimate, RiskLevel risk_level) {
   return estimate * (1 + incremental_step * static_cast<std::underlying_type_t<RiskLevel>>(risk_level));
-}
-
-double BoundingBox::GetLowerBound(const double estimate, RiskLevel risk_level) {
-  return estimate * (1 - decremental_step * static_cast<std::underlying_type_t<RiskLevel>>(risk_level));
 }
